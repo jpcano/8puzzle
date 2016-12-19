5498e89fed3b297f49e56451ec07093febe7e270
Jesus Caño <jcanovel@gmail.com>
2016-12-19 18:03:41 +0100

    Working version for small moves

--- Solver.java
+++ Solver.java
@@ -7,12 +7,15 @@ import edu.princeton.cs.algs4.MinPQ;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.ST;
 import java.util.Comparator;
 
 public class Solver {
 
     private Node problem;
     private int moves = 0;
+    boolean solvable = true;
+    private Node solution = null;
 
     public Solver (Board initial) {
         if (initial == null)
@@ -20,7 +23,7 @@ public class Solver {
         problem = new Node(initial, 0, null);
     }
 
-    private class Node {
+    private class Node implements Comparable<Node> {
         private Board board;
         private int moves;
         private Node prev;
@@ -29,6 +32,15 @@ public class Solver {
             this.board = board;
             this.moves = moves;
             this.prev = prev;
+        }
+
+        public int compareTo(Node that) {
+            if (this.moves < that.moves)
+                return -1;
+            else if (this.moves > that.moves)
+                return 1;
+            else
+                return 0;
         }
     }
 
@@ -47,57 +59,92 @@ public class Solver {
     }
     
     public boolean isSolvable() {
-        return solve() ==  null ? false : true;
+        Node result;
+        result = solve();
+        return result != null ? true : false;
     }
 
     public int moves() {
-        Node solution;
-        solution = solve();
-        return solution.moves;
+        Node result;
+        result = solve();
+        return result != null ? result.moves : -1;
     }
 
     public Iterable<Board> solution() {
         Node actualNode;
-        Stack<Board> solution = new Stack<Board>();
+        Stack<Board> result = new Stack<Board>();
 
         actualNode = solve();
         while (actualNode != null) {
-            solution.push(actualNode.board);
-            actualNode = actualNode.prev;
+                result.push(actualNode.board);
+                actualNode = actualNode.prev;
         }
-        return solution;
+        return result;
     }
 
     private Node solve() {
         MinPQ<Node> actualPQ, testPQ;
         Node actualNode,testNode;
-        Board actualBoard, testBoard;
         Node neighbor;
+        ST<Node, int[]> actualBST, testBST;
 
+        if (solvable && solution != null)
+            return solution;
+        if (!solvable)
+            return null;
+
+        actualNode = problem;
         testNode = new Node(problem.board.twin(), 0, null);
         actualPQ = new MinPQ<Node>(priorityOrder());
+        actualPQ.insert(actualNode);
+        actualBST = new ST<Node, int[]>();
         testPQ = new MinPQ<Node>(priorityOrder());
-        actualPQ.insert(problem);
         testPQ.insert(testNode);
-        actualNode = actualPQ.delMin();
-        actualBoard = actualNode.board;
-        testNode = testPQ.delMin();
-        testBoard = testNode.board;
-        while (!actualBoard.isGoal() && !testBoard.isGoal()) {
-            for (Board b: actualBoard.neighbors()) {
-                neighbor = new Node(b, actualNode.moves + 1, actualNode);
-                actualPQ.insert(neighbor);
+        testBST = new ST<Node, int[]>();
+
+        actualBST.put(actualNode, null);
+        testBST.put(testNode, null);
+        while (true) {
+            if (actualNode != null) {
+                if (actualNode.board.isGoal())
+                    break;
+                actualNode = searchNode(actualNode, actualPQ, actualBST);
             }
-            for (Board b: testBoard.neighbors()) {
-                neighbor = new Node(b, testNode.moves + 1, testNode);
-                testPQ.insert(neighbor);
-            }
-            actualNode = actualPQ.delMin();
-            actualBoard = actualNode.board;
-            testNode = testPQ.delMin();
-            testBoard = testNode.board;
+            else break;
+            if (testNode != null) {
+                if (testNode.board.isGoal())
+                    break;
+                testNode = searchNode(testNode, testPQ, testBST);
+            }      
         }
-        return actualBoard.isGoal() ? actualNode : null;
+
+        if (actualNode != null && actualNode.board.isGoal()) {
+            solution = actualNode;
+            return actualNode;
+        }
+        else {
+            solvable = false;
+            return null;
+        }
+    }
+    
+    private Node searchNode (Node root, MinPQ<Node> pq, ST<Node, int[]> bst) {
+        Node actualNode;
+        Node neighbor;
+        Board actualBoard;
+
+        actualNode = root;
+        actualBoard = actualNode.board;
+        for (Board b: actualBoard.neighbors()) {
+            neighbor = new Node(b, actualNode.moves + 1, actualNode);
+            if (!bst.contains(neighbor)) {
+                pq.insert(neighbor);
+                bst.put(neighbor, null);
+            }
+        }
+        if (pq.isEmpty())
+            return null;
+        return pq.delMin();
     }
     
     public static void main (String[] args) { 
